d2e4771a0b6fbba9b4eef11f3bfe4b917c4beac1
ebraheem <ebraheem20012020@gmail.com>
2025-12-11 14:17:08 +0200

    [UPDATE] store lexemes in parser factory class

--- src/frontend/parsers/ddl_parser_factory.ts
+++ src/frontend/parsers/ddl_parser_factory.ts
@@ -6,20 +6,26 @@ import { DropStatementParser } from "./ddl/drop_statement_parser";
 import { TruncateStatementParser } from "./ddl/truncate_statement_parser";
 
 export class DDLParserFactory {
-    public build(lexemes: Token[]) : Parser {
-        let peek: Token = lexemes[0];
+    private _lexemes: Token[];
+
+    constructor(lexemes: Token[]) {
+        this._lexemes = lexemes;
+    }
+
+    public build() : Parser {
+        let peek: Token = this._lexemes[0];
         if (peek.type !== TokenType.KEYWORD) {
             throw new Error(`syntax error: unexpected ${peek.value}, expected a KEYWORD`)
         }
         switch (peek.value) {
             case 'CREATE':
-                return new CreateStatementParser(lexemes);
+                return new CreateStatementParser(this._lexemes);
             case 'ALTER':
-                return new AlterStatementParser(lexemes);
+                return new AlterStatementParser(this._lexemes);
             case 'DROP':
-                return new DropStatementParser(lexemes);
+                return new DropStatementParser(this._lexemes);
             case 'TRUNCATE':
-                return new TruncateStatementParser(lexemes);
+                return new TruncateStatementParser(this._lexemes);
             default:
                 throw new Error(`syntax error: unexpected token '${peek.value}', expected a KEYWORD`);
         }
