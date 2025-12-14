e8ac69fc5f48bf7aaf9f9196437380cc8a4f8aee
ebraheem <ebraheem20012020@gmail.com>
2025-12-14 14:56:21 +0200

    [ADD] accept null literal in logical condition ast

--- src/interfaces/dml/logical_condition_ast.ts
+++ src/interfaces/dml/logical_condition_ast.ts
@@ -3,6 +3,6 @@ import { ASTNode } from "../ast";
 export interface LogicalConditionStatement extends ASTNode { // i'm gonna need recursion to parse this shit
     type: "LogicalCondition"
     operator: string,
-    left: LogicalConditionStatement | string | number | boolean,
-    right: LogicalConditionStatement | string | number | boolean
+    left: LogicalConditionStatement | string | number | boolean | null,
+    right: LogicalConditionStatement | string | number | boolean | null
 }
