9b57362156b0d60d76086ab6e842bbec916d2fac
ebraheem <ebraheem20012020@gmail.com>
2025-12-15 11:04:49 +0200

    [REFACTOR] core service

--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { Lexer } from 'src/frontend/lexer';
-import { Parser } from 'src/frontend/parser';
 import { DDLParserFactory } from 'src/frontend/parsers/ddl_parser_factory';
 import { DMLParserFactory } from 'src/frontend/parsers/dml_parser_factory';
 import { ASTNode } from 'src/interfaces/ast';
@@ -11,21 +10,7 @@ import { token_array_split } from 'src/utils/token_array_split';
 export class CoreService {
     public execute(buffer: string, type: "DDL" | "DML" = "DDL") {
         let lexemes: Array<Token> = this.lex(buffer);
-        let splitted_lexemes: Array<Array<Token>> = token_array_split(lexemes, { type: TokenType.SEMICOLON });
-        if (splitted_lexemes[splitted_lexemes.length - 1][0].type === TokenType.EOF)
-            splitted_lexemes = splitted_lexemes.slice(0, splitted_lexemes.length - 1)
-        let ASTs: Array<ASTNode> = new Array<ASTNode>();
-        splitted_lexemes.forEach(element => { 
-            switch (type) {
-                case "DDL":
-                    ASTs.push(this.parse_ddl(element));
-                    break;
-                case "DML":
-                    ASTs.push(this.parse_dml(element));
-                    break;
-                }
-        });
-        return ASTs;
+        return type === "DDL" ? this.parse_ddl(lexemes) : this.parse_dml(lexemes);
     }
 
     private lex(buffer: string) : Token[] {
@@ -33,13 +18,25 @@ export class CoreService {
         return lexer.tokenize();
     }
 
-    private parse_ddl(lexems: Token[]) : ASTNode {
-        let parser: Parser = new DDLParserFactory(lexems).build();
-        return parser.parse();
+    private parse_ddl(lexemes: Token[]) : ASTNode[] {
+        let splitted_lexemes: Array<Array<Token>> = token_array_split(lexemes, { type: TokenType.SEMICOLON });
+        if (splitted_lexemes[splitted_lexemes.length - 1][0].type === TokenType.EOF)
+            splitted_lexemes = splitted_lexemes.slice(0, splitted_lexemes.length - 1)
+        let ASTs: Array<ASTNode> = new Array<ASTNode>();
+        splitted_lexemes.forEach(
+            element => ASTs.push((new DDLParserFactory(element)).build().parse()
+        ));
+        return ASTs;
     }
 
-    private parse_dml(lexems: Token[]) : ASTNode {
-        let parser: Parser = new DMLParserFactory(lexems).build();
-        return parser.parse();
+    private parse_dml(lexemes: Token[]) : ASTNode[] {
+        let splitted_lexemes: Array<Array<Token>> = token_array_split(lexemes, { type: TokenType.SEMICOLON });
+        if (splitted_lexemes[splitted_lexemes.length - 1][0].type === TokenType.EOF)
+            splitted_lexemes = splitted_lexemes.slice(0, splitted_lexemes.length - 1)
+        let ASTs: Array<ASTNode> = new Array<ASTNode>();
+        splitted_lexemes.forEach(
+            element => ASTs.push((new DMLParserFactory(element)).build().parse()
+        ));
+        return ASTs;
     }
 }
