c12e9861911ba0d053ca0d7f54b21321b7e0feba
ebraheem <ebraheem20012020@gmail.com>
2025-12-18 09:51:01 +0200

    [REFACTOR] use deserialization methods

--- src/backend/executers/ddl/drop_executer.ts
+++ src/backend/executers/ddl/drop_executer.ts
@@ -1,5 +1,13 @@
 import { Executer } from "src/backend/executer";
-import { premitive } from "src/interfaces/catalog";
+import { 
+    AttributeCatalog, 
+    premitive, 
+    RelationCatalog,
+    ATTRIBUTE_SCHEMA_FILE, 
+    ATTRIBUTE_CATALOG_DATATYPES, 
+    RELATION_CATALOG_DATATYPES, 
+    RELATION_SCHEMA_FILE
+} from "src/interfaces/catalog";
 import { DropStatement } from "src/interfaces/ddl/drop_statement_ast";
 import { ExecutionResult } from "src/interfaces/execution_result";
 
@@ -11,18 +19,17 @@ export class DropExecuter extends Executer {
             }
         }
         let buffer: premitive[][] = [];
-        for await (const row of this._file_handler.stream_read_async('database/schema/relations.csv', ['TEXT'])) {
-            if (statement.objects.includes(row[0])) continue;
+        for await (const row of this._file_handler.stream_read_async(RELATION_SCHEMA_FILE, RELATION_CATALOG_DATATYPES)) {
+            const relation: RelationCatalog = this._analyzer.deserialize_relation(row);
+            if (statement.objects.includes(relation.name)) continue;
             buffer.push(row as premitive[]);
         }
         await this._file_handler.write_async('database/schema/relations.csv', buffer);
         buffer = []
-        for await (const row of this._file_handler.stream_read_async(
-            'database/schema/attributes.csv', 
-            ['TEXT', 'TEXT', 'TEXT', 'BOOL', 'BOOL', 'TEXT', 'BOOL', 'BOOL', 'TEXT']
-        )) {
-            if (statement.objects.includes(row[0])) continue;
-            buffer.push(row as premitive[]);
+        for await (const row of this._file_handler.stream_read_async(ATTRIBUTE_SCHEMA_FILE, ATTRIBUTE_CATALOG_DATATYPES)) {
+            const attribute: AttributeCatalog = this._analyzer.deserialize_attribute(row);
+            if (statement.objects.includes(attribute.relation)) continue;
+            buffer.push(row);
         }
         await this._file_handler.write_async('database/schema/attributes.csv', buffer);
         await this._file_handler.delete_dirs_async(statement.objects.map(o => `database/data/${o}`));
