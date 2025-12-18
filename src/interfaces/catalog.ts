8d238d1e1b6b68627141fd04116c12a5e219b3a9
ebraheem <ebraheem20012020@gmail.com>
2025-12-18 09:11:48 +0200

    [ADD] datatypes of catalogs to store them in a single place

--- src/interfaces/catalog.ts
+++ src/interfaces/catalog.ts
@@ -3,12 +3,14 @@ export type premitive = string | boolean | number | null;
 export type data_type = 'INT' | 'TEXT' | 'BOOL' | 'SERIAL';
 
 export interface RelationCatalog {
-    name: string
+    name: string,
+    row_count: number
 }
 
 export interface AttributeCatalog {
     relation: string,
     name: string,
+    index: number,
     type: data_type,
     not_null: boolean,
     unique: boolean,
@@ -17,3 +19,6 @@ export interface AttributeCatalog {
     fk: boolean,
     reference?: string
 }
+
+export const RelationCatalogDataTypes: data_type[] = ['TEXT', 'INT']
+export const AttributeCatalogDataTypes: data_type[] = ['TEXT', 'TEXT', 'INT', 'TEXT', 'BOOL', 'BOOL', 'TEXT', 'BOOL', 'BOOL', 'TEXT'];
