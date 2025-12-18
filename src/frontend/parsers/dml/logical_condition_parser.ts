a293740ff05f79be8f4b267f312d4fe5f8e6a1dd
ebraheem <ebraheem20012020@gmail.com>
2025-12-18 14:15:24 +0200

    [ADD] token validation while parsing logical condition

--- src/frontend/parsers/dml/logical_condition_parser.ts
+++ src/frontend/parsers/dml/logical_condition_parser.ts
@@ -1,16 +1,25 @@
 import { Parser } from "src/frontend/parser";
 import { premitive } from "src/interfaces/catalog";
 import { LogicalConditionStatement } from "src/interfaces/dml/logical_condition_ast";
-import { TokenType } from "src/interfaces/token";
+import { Token, TokenType } from "src/interfaces/token";
 
 export class LogicalConditionParser extends Parser {
     public parse() : LogicalConditionStatement {
         this.consume(TokenType.KEYWORD, 'WHERE');
+        const left: Token = this.consume(TokenType.IDENTIFIER);
+        if (typeof(left.value) !== "string")
+            throw new SyntaxError(`unexpected token ${left.value}, expected identifier`);
+        const operator: Token = this.consume(TokenType.OPERATOR);
+        if (typeof(operator.value) !== "string")
+            throw new SyntaxError(`unexpected token ${left.value}, expected identifier`);
+        const right: Token = this.consume();
+        if (![TokenType.STRING, TokenType.NUMBER, TokenType.NULL, TokenType.BOOLEAN].includes(right.type))
+            throw new SyntaxError(`unexpected token ${left.value}, expected a literal`);
         return {
             type: "LogicalCondition",
-            left: this.consume(TokenType.IDENTIFIER).value as string,
-            operator: this.consume(TokenType.OPERATOR).value as string,
-            right: this.consume().value as premitive
+            left: left.value,
+            operator: operator.value,
+            right: right.value as premitive
         }
     }
 }
