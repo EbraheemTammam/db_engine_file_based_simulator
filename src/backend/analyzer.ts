7db2b2aac3ce90087d3082c57b66d9ffdb510515
ebraheem <ebraheem20012020@gmail.com>
2025-12-18 08:39:22 +0200

    [ADD] method to check column existance

--- src/backend/analyzer.ts
+++ src/backend/analyzer.ts
@@ -9,9 +9,18 @@ export class Analyzer {
     }
 
     public async check_table_existance(table_name: string): Promise<boolean> {
-        for await (const row of this._file_handler.stream_read_async('database/schema/relations.csv', ['TEXT'])) {
-            if (row[0] === table_name) return true;
-        }
+        for await (const row of this._file_handler.stream_read_async(
+            'database/schema/relations.csv', 
+            ['TEXT', 'INT']
+        )) if (row[0] === table_name) return true;
+        return false;
+    }
+    
+    public async check_column_existance(table_name: string, column_name: string) {
+        for await (const row of this._file_handler.stream_read_async(
+            'database/schema/attributes.csv', 
+            ['TEXT', 'TEXT', 'TEXT', 'BOOL', 'BOOL', 'TEXT', 'BOOL', 'BOOL', 'TEXT']
+        )) if (row[0] === table_name && row[1] === column_name) return true;
         return false;
     }
 }
