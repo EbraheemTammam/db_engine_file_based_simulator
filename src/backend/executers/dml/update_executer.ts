7ab6b35655c79b393fad62813fde6376625cf02b
ebraheem <ebraheem20012020@gmail.com>
2025-12-22 15:53:54 +0200

    [UPDATE] update statement executer to execute range queries

--- src/backend/executers/dml/update_executer.ts
+++ src/backend/executers/dml/update_executer.ts
@@ -1,6 +1,6 @@
 import { Executer } from "src/backend/executer";
 import { TABLE_PAGE_DATA_FILE } from "src/constants/file_path";
-import { AttributeCatalog, premitive, RelationCatalog } from "src/interfaces/catalog";
+import { AttributeCatalog, data_type, premitive, RelationCatalog } from "src/interfaces/catalog";
 import { UpdateStatement } from "src/interfaces/dml/update_statement_ast";
 import { ExecutionResult } from "src/interfaces/execution_result";
 
@@ -11,63 +11,56 @@ export class UpdateExecuter extends Executer {
             throw new Error('multiple assignments to the same column');
         // validate expressions match columns datatypes
         await this._analyzer.validate_column_datatypes_async(statement.table, statement.columns, [statement.values]);
-        let row_count: number = 1;
-        switch (statement.condition) {
-            case undefined:
-                row_count = await this.update_all_async(statement);
-                break;
-            default:
-                await this.update_one_async(statement);
-        }
-        return { type: "COMMAND", tag: "UPDATE", row_count: row_count }
-    }
-
-    private async update_one_async(statement: UpdateStatement): Promise<void> {
-        const id: number = statement.condition?.right as number;
-        const page_number: number = this._analyzer.get_page_number(id);
-        const page_count: number = (await this._analyzer.get_relation_catalog_async(statement.table)).page_count;
-        if (page_number > page_count)
-            throw new Error(`object with id ${id} does not exist`);
         // validating nullability of columns
         const attrs_catalogs: AttributeCatalog[] = await this._analyzer.get_attributes_catalogs_async(
             statement.table, statement.columns
         );
         const values: premitive[] = this._analyzer.validate_not_null(attrs_catalogs, [statement.values])[0];
-        const buffer: premitive[][] = [];
-        const indexes: number[] = await this._analyzer.get_column_indexes_async(statement.table, statement.columns);
-        let found = false;
-        for await (const row of this._file_handler.stream_read_async(TABLE_PAGE_DATA_FILE(statement.table, page_number))) {
-            if (Number(row[0]) === id) {
-                found = true;
-                for (let i: number = 0; i < values.length; ++i)
-                    row[indexes[i]] = values[i];
-                buffer.push(row);
-                continue;
-            }
-            buffer.push(row);
-        }
-        if (!found) 
-            throw new Error(`object with id ${id} does not exist`); 
-        await this._file_handler.write_async(TABLE_PAGE_DATA_FILE(statement.table, page_number), buffer);
-    }
-
-    private async update_all_async(statement: UpdateStatement):  Promise<number> {
+        // get the schema of the relation to parse while reading data
         const catalog: RelationCatalog = await this._analyzer.get_relation_catalog_async(statement.table);
+        const schema: data_type[] = (
+            await this._analyzer.get_table_attributes_catalogs_async(catalog.name)
+        ).map(attr => attr.type);
+        // calculate start page, end page
+        const boundaries: [number, number] = await this._analyzer.calculate_range_query_pages_async(catalog.name, statement.condition);
+        // construct lambda to check matching rows
+        const is_match = await this._analyzer.construct_logical_lambda_async(catalog.name, statement.condition);
+        // get the index of the pivot column (provided identifier in where clause)
+        const identifier_index: number = (
+            await this._analyzer.get_column_indexes_async(
+                catalog.name,
+                statement.condition === undefined ?
+                [] :
+                [statement.condition?.left as string]
+            )
+        )[0];
+        // get indexes of provided columns
         const indexes: number[] = await this._analyzer.get_column_indexes_async(statement.table, statement.columns);
-        // validating nullability of columns
-        const attrs_catalogs: AttributeCatalog[] = await this._analyzer.get_attributes_catalogs_async(
-            statement.table, statement.columns
-        );
-        const values: premitive[] = this._analyzer.validate_not_null(attrs_catalogs, [statement.values])[0];
-        for (let page_number: number = 1; page_number <= catalog.page_count; ++page_number) {
+        // store affected row count
+        let count: number = 0;
+        // loop over calculated pages
+        for (let page_number: number = boundaries[0]; page_number <= boundaries[1]; ++page_number) {
+            // construct updated buffer
             const buffer: premitive[][] = [];
-            for await (const row of this._file_handler.stream_read_async(TABLE_PAGE_DATA_FILE(statement.table, page_number))) {
-                for (let i: number = 0; i < values.length; ++i)
-                    row[indexes[i]] = values[i];
+            for await (const row of this._file_handler.stream_read_async(
+                TABLE_PAGE_DATA_FILE(statement.table, page_number),
+                schema
+            )) {
+                // check if the value of the pivot column matching the one provided in where clause
+                if (is_match(row[identifier_index], statement.condition?.right as premitive)) {
+                    // if so update the row with the provided values
+                    for (let i: number = 0; i < values.length; ++i)
+                        row[indexes[i]] = values[i];
+                    buffer.push(row);
+                    ++count;
+                    continue;
+                }
+                // else take the row as it is
                 buffer.push(row);
-            } 
+            }
+            // write the constructed buffer to the current page
             await this._file_handler.write_async(TABLE_PAGE_DATA_FILE(statement.table, page_number), buffer);
         }
-        return catalog.row_count;
+        return { type: "COMMAND", tag: "UPDATE", row_count: count }
     }
 }
