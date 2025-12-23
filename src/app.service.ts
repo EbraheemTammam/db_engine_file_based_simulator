2e901e187cd01ebf27c9e386642b6f497c2eb1f4
ebraheem <ebraheem20012020@gmail.com>
2025-12-23 09:24:39 +0200

    [ADD] startup event to create initial dirs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { IFileHandler } from './interfaces/file_handler';
+import { FileHandler } from './backend/file_handler';
+import { ATTRIBUTE_SCHEMA_FILE, RELATION_SCHEMA_FILE } from './constants/file_path';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+export class AppService implements OnApplicationBootstrap {
+	async onApplicationBootstrap() {
+		const handler: IFileHandler = new FileHandler();
+		await handler.make_dir("database/schema");
+		await handler.make_dir("database/data");
+		await handler.append_async(RELATION_SCHEMA_FILE, []);
+		await handler.append_async(ATTRIBUTE_SCHEMA_FILE, []);
+	}
 }
